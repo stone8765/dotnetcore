2e4d133399bc7385c96852e0a62e5fc1bad4580e
StoneLi <lileisayno@live.com>
2019-11-18 23:30:36 +0800

    add project OCR_Test

---
+++ OCR_Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Tesseract;
+
+namespace OCR_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testImage = AppDomain.CurrentDomain.BaseDirectory + "test_image\\a.png";
+            try
+            {
+                using (var engine =new TesseractEngine(@"tessdata","eng+chi_sim",EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(testImage))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var texto = page.GetText();
+                            Console.WriteLine(":{0}", page.GetMeanConfidence());
+                            Console.WriteLine("Texto:{0}", texto);
+                        }
+                            
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
